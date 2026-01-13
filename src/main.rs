d4bbf6bfa6b7ae6acfda6f1ad8dc3d3c9312d2a4
Josh Koter <josh.koter@gmail.com>
2026-01-13 22:22:55 +0200

    type conversion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// get the io library from the standard library
+// this is kind of like namespaces in
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number game 🤔");
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    println!("Input your guess");
+
+    // mut makes the variable reassignable/ mutable
+    // by default they're immutable
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("type a number foo'");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
